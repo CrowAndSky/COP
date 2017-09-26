5094bd92a67e85676dc6b1fdcaa9437599b9ec08
Preston Williams <prwilliams@sherwin.com>
2017-09-26 16:43:08 -0400

    Cleaned up

--- app.js
+++ app.js
@@ -2,13 +2,9 @@
 
 /* ------------------ ### wwwwwwww ### ------------------\
 
-97, 168, 157
 151, 203, 210 SW 9046 Gentle Aquamarine
-
 132, 180, 190 SW 6493 Ebbtide
 153, 160, 178 SW 6542 Vesper Violet
-
-191, 210, 201
 110, 194, 196  SW 6766 Mariner
 
 var styles = getComputedStyle(document.documentElement);
@@ -22,27 +18,31 @@ var fooBar = bodyStyles.getPropertyValue('--foo-bar');
 $(document).ready( function(){
 
 const $appWrapper = $( '#app-wrapper' );
-let loopState = "top";
+let loopState = "top",
+    pauseTransitions = false;
 
-window.setInterval( function(){
-    switch ( loopState ) {
-    case "top":
-        $appWrapper.attr( 'class', 'state-detail' );
-        loopState = 'detail';
-        break;
-    case "detail":
-        $appWrapper.attr( 'class', 'state-lower-room' );
-        loopState = 'lower';
-        break;
-    case "lower":
-        $appWrapper.attr( 'class', 'state-lower-room state-detail' );
-        loopState = 'bottomDetail';
-        break;
-    case "bottomDetail":
-        $appWrapper.attr( 'class', '' );
-        loopState = 'top';
-        break;
+window.setInterval( function() {
+    if ( !pauseTransitions ) {
+        switch ( loopState ) {
+            case "top":
+                $appWrapper.attr( 'class', 'state-detail' );
+                loopState = 'detail';
+                break;
+            case "detail":
+                $appWrapper.attr( 'class', 'state-lower-room' );
+                loopState = 'lower';
+                break;
+            case "lower":
+                $appWrapper.attr( 'class', 'state-lower-room state-detail' );
+                loopState = 'bottomDetail';
+                break;
+            case "bottomDetail":
+                $appWrapper.attr( 'class', '' );
+                loopState = 'top';
+                break;
+        }
     }
-}, 2000);
+}, 8000);
 
+$appWrapper.click( function(){ pauseTransitions = !pauseTransitions; });
 });
